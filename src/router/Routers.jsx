4587af96dcb34629a83782a653697e19ce88252b
debtx <sankad@slt.com.lk>
2025-02-04 19:25:00 +0530

    set correct folder structure in Drc repo

--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -10,34 +10,11 @@ import PrototypeA from "../assets/prototype/prototypeA";
 import PrototypeB from "../assets/prototype/prototypeB";
 import PrototypeC from "../assets/prototype/prototypeC";
 
-import RecoveryOfficerRequests from "../pages/Request/additiional_request_log";
-
-import ForwardMediationBoard from "../pages/Request/ForwardMediationBoard";
-import ValidityPeriodExtension from "../pages/Request/ValidityPeriodExtension";
-import RequestResponseLog from "../pages/Request/RequestResponseLog";
-
-import Case_List from "../pages/Incident/Case_List";
-import Incident_List from "../pages/Incident/Incident_List";
-import CaseDetails from "../pages/Incident/Case_Details";
-import Incident_Register_Individual from "../pages/Incident/Incident_Register_Individual";
-import SupBulkUploadLog from "../pages/Incident/sup_bulk_upload_LOG";
-import Incident_Register_Bulk_Upload from "../pages/Incident/Incident_Register_Bulk_Upload";
-import Incident_File_Download from "../pages/Incident/Incident_File_Download";
-import PageButton from "../pages/Incident/page_button";
-
  {/* Distribute Imports */}
- import AssignDRC from "../pages/Distribute/AssignDRC";
- import AssignDRCForCollectCPE from "../pages/Distribute/AssignDRCForCollectCPE";
- import AssignDRCsLOG from "../pages/Distribute/AssignDRCsLOG";
- import AssignedDRCSummary from "../pages/Distribute/AssignedDRCSummary";
- import AssignedDRCSummaryCollectCPE from "../pages/Distribute/AssignedDRCSummaryCollectCPE";
- import DistributionPreparationBulkUpload from "../pages/Distribute/DistributionPreparationBulkUpload";
- import DistributionPreparationOnlyCPECollect from "../pages/Distribute/DistributionPreparationOnlyCPECollect";
- import DRCAssignManagerApproval from "../pages/Distribute/DRCAssignManagerApproval";
- import IncidentFilter from "../pages/Distribute/IncidentFilter";
- import ReAssignDRC from "../pages/Distribute/ReAssignDRC";
- import DistributeDummy from "../pages/Distribute/DistributeDummy";
- import DistributeTORO from "../pages/Distribute/DistributeTORO";
+ 
+import DistributeTORO from "../pages/Distribute/DistributeTORO";
+
+{/* DRC Imports */}
 
 import Dummy from "../pages/DRC/Dummy";
 import AssignedCaseListforDRC from "../pages/DRC/Assigned case list for DRC";
@@ -68,35 +45,8 @@ const Routers = () => {
       <Route path="/prototypeB" element={<ProtectedRoute element={<PrototypeB />} allowedRoles={['superadmin']} />} />
       <Route path="/prototypeC" element={<ProtectedRoute element={<PrototypeC />} allowedRoles={['superadmin']} />} />
 
-      <Route path="/additional_request_log" element={<ProtectedRoute element={<RecoveryOfficerRequests />} allowedRoles={['superadmin']} />} />
-
-      <Route path="/drc/mediation-board" element={<ProtectedRoute element={<ForwardMediationBoard />} allowedRoles={['superadmin']} />} />
-      <Route path="/drc/period-extension" element={<ProtectedRoute element = {<ValidityPeriodExtension/>} allowedRoles={['superadmin']} />} />
-      <Route path="/drs/logs/reuest-response-logs" element={<ProtectedRoute element = {<RequestResponseLog/>} allowedRoles={['superadmin']} />}/>
-
-      {/* incient team4 */}
-      <Route path="/incident" element={<ProtectedRoute element={<PageButton />} allowedRoles={['superadmin']} />} />
-      <Route path="/Incident/Case_List" element={<ProtectedRoute element={<Case_List />} allowedRoles={['superadmin']} />} />
-      <Route path="/Incident/Incident_List" element={<ProtectedRoute element={<Incident_List />} allowedRoles={['superadmin']} />} />
-      <Route path="/Incident/Case_Details" element={<ProtectedRoute element={<CaseDetails />} allowedRoles={['superadmin']} />} />
-      <Route path="/incident/register" element={<ProtectedRoute element={<Incident_Register_Individual />} allowedRoles={['superadmin']} />} />
-      <Route path="/incident/upload-log" element={<ProtectedRoute element={<SupBulkUploadLog />} allowedRoles={['superadmin']} />} />
-      <Route path="/incident/register-bulk" element={<ProtectedRoute element={<Incident_Register_Bulk_Upload />} allowedRoles={['superadmin']} />} />
-      <Route path="/incident/file-download" element={<ProtectedRoute element={<Incident_File_Download />} allowedRoles={['superadmin']} />} />
-
      {/* Distribute Routes */}
-     <Route path="/pages/Distribute/AssignDRC" element={<ProtectedRoute element={<AssignDRC/>} allowedRoles={['superadmin']} />} />
-     <Route path="/pages/Distribute/AssignDRCForCollectCPE" element={<ProtectedRoute element={<AssignDRCForCollectCPE/>} allowedRoles={['superadmin']} />} />
-     <Route path="/pages/Distribute/AssignDRCsLOG" element={<ProtectedRoute element={<AssignDRCsLOG />} allowedRoles={['superadmin']} />} />
-     <Route path="/pages/Distribute/AssignedDRCSummary" element={<ProtectedRoute element={<AssignedDRCSummary />} allowedRoles={['superadmin']} />} />
-     <Route path="/pages/Distribute/AssignedDRCSummaryCollectCPE" element={<ProtectedRoute element={<AssignedDRCSummaryCollectCPE/>} allowedRoles={['superadmin']} />} />
-     <Route path="/pages/Distribute/DistributionPreparationBulkUpload" element={<ProtectedRoute element={<DistributionPreparationBulkUpload />} allowedRoles={['superadmin']} />} />
-     <Route path="/pages/Distribute/DistributionPreparationOnlyCPECollect" element={<ProtectedRoute element={<DistributionPreparationOnlyCPECollect />} allowedRoles={['superadmin']} />} />
-     <Route path="/pages/Distribute/DRCAssignManagerApproval" element={<ProtectedRoute element={<DRCAssignManagerApproval />} allowedRoles={['superadmin']} />} />
-     <Route path="/pages/Distribute/IncidentFilter" element={<ProtectedRoute element={<IncidentFilter />} allowedRoles={['superadmin']} />} />
-     <Route path="/pages/Distribute/DistributeDummy" element={<ProtectedRoute element={<DistributeDummy />} allowedRoles={['superadmin']} />} />
-     <Route path="/pages/Distribute/ReAssignDRC" element={<ProtectedRoute element={<ReAssignDRC />} allowedRoles={['superadmin']} />} />
-     <Route path="/pages/Distribute/DistributeTORO" element={<ProtectedRoute element={<DistributeTORO />} allowedRoles={['superadmin']} />} />
+     
      <Route path="/pages/Distribute/DistributeTORO/:drc_id" element={<ProtectedRoute element={<DistributeTORO />} allowedRoles={['superadmin']} />} />
 
       {/* DRC Routes */}
