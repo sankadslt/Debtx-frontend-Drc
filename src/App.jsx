c430d9f2a052f397da33f0a0ddb94b29a6c56524
ShyamalWarnakula <122584532+Warnakulaisuru@users.noreply.github.com>
2025-07-28 10:50:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename="/drc">
+    <BrowserRouter basename="/test/drc">
       <Layout />
     </BrowserRouter>
   );
